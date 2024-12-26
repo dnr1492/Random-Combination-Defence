155454cadf33e81917b3ea96d859c9c1b11ea87e
dnr1492 <dnr1492@gmail.com>
2024-12-26 12:09:53 +0900

    [Chore] SPUM Asset - Download & Import

--- Assets/AssetStore/SPUM/Script/Ect/ColorSelect.cs
+++ Assets/AssetStore/SPUM/Script/Ect/ColorSelect.cs
@@ -1,38 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ColorSelect : MonoBehaviour
 {
-    #if UNITY_EDITOR
     public int index;
     public Image _savedColor;
 
+    #if UNITY_EDITOR
     public void SetColorSave()
     {
-        SoonsoonData.Instance._spumManager._nowSelectColorNum = index;
-        if(_savedColor.gameObject.activeInHierarchy)
+        SoonsoonData.Instance._spumManager.UIManager._nowSelectColorNum = index; // 인덱스 전달
+        if(_savedColor.gameObject.activeInHierarchy) // 활성화 상태면
         {
-            SoonsoonData.Instance._spumManager._nowColor = _savedColor.color;
-            SoonsoonData.Instance._spumManager._nowColorShow.color = _savedColor.color;
-            SoonsoonData.Instance._spumManager._hexColorText.text = SoonsoonData.Instance._spumManager.ColorToStr(_savedColor.color);
-            SoonsoonData.Instance._spumManager.SetObjColor();
-
-            SoonsoonData.Instance._spumManager.ToastOn("Loaded Color");
-            SoonsoonData.Instance._spumManager._nowSelectColor.SetActive(true);
-            SoonsoonData.Instance._spumManager._nowSelectColor.transform.position = transform.position;
+            SoonsoonData.Instance._spumManager.UIManager.NowColor = _savedColor.color;
+            string parsedColor = ColorUtility.ToHtmlStringRGB(_savedColor.color); 
+            SoonsoonData.Instance._spumManager.UIManager.ToastOn($"Selected Color <color=#{parsedColor}>◆</color>#" + parsedColor);
+            SoonsoonData.Instance._spumManager.UIManager._nowSelectColor.SetActive(true); // 셀럭터 이동
+            SoonsoonData.Instance._spumManager.UIManager._nowSelectColor.transform.position = transform.position;
             //set the color
         }
         else
         {
             //saved the color
             _savedColor.gameObject.SetActive(true);
-            _savedColor.color = SoonsoonData.Instance._spumManager._nowColor;
-            SoonsoonData.Instance._spumManager.ToastOn("Saved Color");
-
-            string tSTR = SoonsoonData.Instance._spumManager.ColorToStr(_savedColor.color);
-            SoonsoonData.Instance._soonData2._savedColorList[index] = tSTR;
+            _savedColor.color = SoonsoonData.Instance._spumManager.UIManager.NowColor;
+            string parsedColor = ColorUtility.ToHtmlStringRGB(_savedColor.color); // SoonsoonData.Instance._spumManager.UIManager.ColorToStr(_savedColor.color);
+            SoonsoonData.Instance._spumManager.UIManager.ToastOn($"Saved Color <color=#{parsedColor}>◆</color>#" + parsedColor);
+            SoonsoonData.Instance._soonData2._savedColorList[index] = "#"+parsedColor;
+            SoonsoonData.Instance._spumManager.UIManager._nowSelectColor.transform.position = transform.position;
             SoonsoonData.Instance.SaveData();
         }
     }
