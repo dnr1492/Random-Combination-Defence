b87bc4d1c539ece1ce69b0803ec153cf036f0f20
dnr1492 <dnr1492@gmail.com>
2025-03-18 11:04:39 +0900

    [Build] 폴더 위치를 Plugins 내부로 이동

--- Assets/08_Custom/EnumClass.cs
+++ Assets/08_Custom/EnumClass.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CharacterDisplayName { 전사, 도적, 마법사, 궁수, 격투가, 광전사, 창술사, 주술사, 사제, 성기사, 마검사, 닌자, 어쌔신, 레인져, 스나이퍼, 파이터, 버서커, 파이크맨, 마창사, 흑마술사, 소서러, 드루이드, 바드 }
+public enum CharacterDisplayName { 전사, 도적, 마법사, 궁수, 격투가, 광전사, 창술사, 주술사, 사제, 성기사, 마검사, 닌자, 어쌔신, 레인저, 스나이퍼, 파이터, 버서커, 파이크맨, 마창사, 흑마술사, 소서러, 드루이드, 바드 }
 
 public enum CharacterTier { None, 흔한, 안흔한, 희귀한, 유일한, 전설적인, 초월한 }
 
